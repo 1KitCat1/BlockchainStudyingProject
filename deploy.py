28dc215949f30a9dd4834fd272452a9fa34bb16a
1KitCat1 <denischool119@gmail.com>
2022-02-06 23:43:41 +0200

    Deploy add creating a contract

--- deploy.py
+++ deploy.py
@@ -41,3 +41,27 @@ with open("./local_web3_data.json", "r") as file:
 
 web3 = Web3(Web3.HTTPProvider(local_web3_data["server"]))
 
+# creating contract
+simpleStorage_contract = web3.eth.contract(abi=abi, bytecode=bytecode)
+
+chain_id = int(local_web3_data["network_id"])
+address = local_web3_data["address"]
+nonce = web3.eth.getTransactionCount(address)
+
+simpleStorage_deploy_trx = simpleStorage_contract.constructor().buildTransaction(
+    {
+        "chainId": chain_id,
+        "from": address,
+        "nonce": nonce,
+        "gasPrice": web3.eth.gas_price,
+    }
+)
+simpleStorage_deploy_trx_signed = web3.eth.account.sign_transaction(
+    simpleStorage_deploy_trx, local_web3_data["private_key"]
+)
+simpleStorage_deploy_trx_hash = web3.eth.send_raw_transaction(
+    simpleStorage_deploy_trx_signed.rawTransaction
+)
+simpleStorage_deploy_trx_receipt = web3.eth.wait_for_transaction_receipt(
+    simpleStorage_deploy_trx_hash
+)
